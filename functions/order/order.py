1dd614221eab2b45780119eda9cf8d5147d0865e
Tomohiro Uemura <t.uemura618@gmail.com>
2019-04-26 01:51:12 +0800

    Add create Order

--- functions/order/order.py
+++ functions/order/order.py
@@ -3,6 +3,13 @@ import boto3
 import json
 import decimal
 import uuid
+import decimal
+from datetime import datetime
+
+# Table name
+TABLE_NAME = "SagaEcOrder"
+dynamodb = boto3.resource('dynamodb').Table(TABLE_NAME)
+
 
 def lambda_handler(event, context):
 
@@ -12,20 +19,52 @@ def lambda_handler(event, context):
     print(itemName)
     print(price)
 
-    _insert_dynamo(str(uuid.uuid4()), event)
+    _insert_dynamo(str(uuid.uuid4()), itemName)
+
+def get(event, context):
+
+    # print(event["pathParameters"]["txId"])
+    res = dynamodb.get_item(
+        Key={
+            "TxId" : event["pathParameters"]["txId"]
+        }   
+    )   
+    return { 'body' : str(res) }
 
 
-def _insert_dynamo(txId, ):
-    dynamo = boto3.resource('dynamodb').Table("SagaEcOrder")
+def create(event, context):
+    req = event # json.loads(event["body"]) if type(event["body"]) == str else event["body"]
+    orderNo = "OR-"+ datetime.now().isoformat()
+    
+    res = dynamodb.put_item(
+        Item = {
+            "TxId": str(uuid.uuid4()),
+            "OrderNo": orderNo,
+            "ItemName": req["itemName"],
+            "Price": decimal.Decimal(req["price"])
+        }
+        # ,
+        # UpdateExpression='set OrderNo = OrderNo + :val',
+        # ExpressionAttributeValues={
+        #     ':val': 1
+        # },
+        # ReturnValues='UPDATED_NEW'        }  
+    )
+#    print(Item)
+    # print(json.dumps(res, indent=4, cls=DecimalEncoder))
+    return { 'body' : str(res) }
+
+def _insert_dynamo(txId, itemName):
+    dynamo = boto3.resource('dynamodb').Table(TABLE_NAME)
 
     json_str = '''
     {
-        "name": "btc",
-        "price": %f
+        "txId": txId,
+        "itemName": itemName
     }
     ''' % (price)
 
-    item = json.loads(json_str, parse_float=decimal.Decimal)
-    dynamo.put_item(Item = item)
+    # item = json.loads(json_str, parse_float=decimal.Decimal)
+    # dynamo.put_item(Item = item)
 
     return {"status": "OK"}
